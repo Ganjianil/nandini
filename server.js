d221128435996b08c06cad8a4eb01a496ae8a26a
Ganjianil <ganjianil@outlook.com>
2025-06-25 11:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const app=express();
-const port = process.env.PORT || 29112;
+const port = process.env.PORT || 10406;
 const mysql= require("mysql2")
 const multer= require("multer")
 const upload=multer({dest:'upload/'})
