0a1092ea0777cb10329fc4cc1c8d220ec69b3cee
Ganjianil <ganjianil@outlook.com>
2025-06-25 15:57:03 +0530

    Update AddProduct.jsx

--- frontend/app/src/AddProduct.jsx
+++ frontend/app/src/AddProduct.jsx
@@ -39,7 +39,7 @@ const AddProduct = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8085/products",
+        "http://localhost:10406/products",
         formDataToSend,
         {
           headers: {
